87bc92418cc9f1663b8e50e666f50b9be0fc0e0d
StasPtichka <lenny.bonus111@gmail.com>
2017-03-11 12:56:19 +0300

    date_of_born,name,surname_into_firebase_database

--- createPage.js
+++ createPage.js
@@ -1,46 +1,61 @@
 //Стас
-//  var config = {
-//      apiKey: "AIzaSyB5Wgo5RO_GI5xf6fsaSfH8OdZlx-X7lqw",
-//      authDomain: "simpleservice-778ef.firebaseapp.com",
-//      databaseURL: "https://simpleservice-778ef.firebaseio.com",
-//     storageBucket: "simpleservice-778ef.appspot.com",
-//     messagingSenderId: "698110095322"
-//  };
-// firebase.initializeApp(config);
-
-//Дима
 var config = {
-    apiKey: "AIzaSyAzreIAAimmDA0-jAiqwtQZdx9gasNKONw",
-    authDomain: "booking-48d90.firebaseapp.com",
-    databaseURL: "https://booking-48d90.firebaseio.com",
-    storageBucket: "booking-48d90.appspot.com",
-    messagingSenderId: "272973760259"
+    apiKey: "AIzaSyB5Wgo5RO_GI5xf6fsaSfH8OdZlx-X7lqw",
+    authDomain: "simpleservice-778ef.firebaseapp.com",
+    databaseURL: "https://simpleservice-778ef.firebaseio.com",
+    storageBucket: "simpleservice-778ef.appspot.com",
+    messagingSenderId: "698110095322"
 };
 firebase.initializeApp(config);
 
-var appHome = angular.module('appHome', []);
+/*Дима
+ var config = {
+ apiKey: "AIzaSyAzreIAAimmDA0-jAiqwtQZdx9gasNKONw",
+ authDomain: "booking-48d90.firebaseapp.com",
+ databaseURL: "https://booking-48d90.firebaseio.com",
+ storageBucket: "booking-48d90.appspot.com",
+ messagingSenderId: "272973760259"
+ };*/
+//firebase.initializeApp(config);
 
-    appHome.controller('Create', function ($scope) {
-        $scope.btnCreate = function () {
-            email = $scope.email;
-            pass = $scope.pass;
-            passRetry = $scope.passRetry;
-            $scope.btn = '';
-            $scope.pass = $scope.btn;
-            $scope.passRetry = $scope.btn;
-            checked = false;
-            $scope.check = true;
+var appCreate = angular.module('appCreate', []);
 
-            if (pass !== passRetry) {
-                alert("Пароли не совпадают!!!")
-            } else {
-                firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function (error) {
-                    if (error.code) {
-                        alert(error.message);
-                    } else {
-                        alert("Регистрация аккаунта прошла успешно!!!")
-                    }
+appCreate.controller('Create', function ($scope) {
+    $scope.btnCreate = function () {
+        email = $scope.email;
+        pass = $scope.pass;
+        name = $scope.name;
+        surname = $scope.surname;
+        dateOfborn = $scope.DOB;
+        passRetry = $scope.passRetry;
+        $scope.btn = '';
+        $scope.pass = $scope.btn;
+        $scope.passRetry = $scope.btn;
+        checked = false;
+        $scope.check = true;
+
+        if (pass !== passRetry) {
+            alert("Пароли не совпадают!!!")
+        } else {
+            firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function (error) {
+                if (error.code) {
+                    alert(error.message);
+                } else {
+                    alert("Регистрация аккаунта прошла успешно!!!")
+                }
+            });
+
+            date_of_born = dateOfborn.toDateString();
+            var bookRef = firebase.database().ref("pasport/" + name);//тут вместо name будет id пользователя
+            bookRef.set(
+                {
+                    Name: name,
+                    Surname: surname,
+                    DOB: date_of_born
                 });
-            }
+
         }
-    });
+
+
+    }
+});
--- homePage.js
+++ homePage.js
@@ -1,36 +1,36 @@
 //Стас
-//  var config = {
-//      apiKey: "AIzaSyB5Wgo5RO_GI5xf6fsaSfH8OdZlx-X7lqw",
-//      authDomain: "simpleservice-778ef.firebaseapp.com",
-//      databaseURL: "https://simpleservice-778ef.firebaseio.com",
-//     storageBucket: "simpleservice-778ef.appspot.com",
-//     messagingSenderId: "698110095322"
-//  };
-// firebase.initializeApp(config);
-
-//Дима
 var config = {
-    apiKey: "AIzaSyAzreIAAimmDA0-jAiqwtQZdx9gasNKONw",
-    authDomain: "booking-48d90.firebaseapp.com",
-    databaseURL: "https://booking-48d90.firebaseio.com",
-    storageBucket: "booking-48d90.appspot.com",
-    messagingSenderId: "272973760259"
+    apiKey: "AIzaSyB5Wgo5RO_GI5xf6fsaSfH8OdZlx-X7lqw",
+    authDomain: "simpleservice-778ef.firebaseapp.com",
+    databaseURL: "https://simpleservice-778ef.firebaseio.com",
+    storageBucket: "simpleservice-778ef.appspot.com",
+    messagingSenderId: "698110095322"
 };
 firebase.initializeApp(config);
 
+/*Дима
+ var config = {
+ apiKey: "AIzaSyAzreIAAimmDA0-jAiqwtQZdx9gasNKONw",
+ authDomain: "booking-48d90.firebaseapp.com",
+ databaseURL: "https://booking-48d90.firebaseio.com",
+ storageBucket: "booking-48d90.appspot.com",
+ messagingSenderId: "272973760259"
+ };*/
+//firebase.initializeApp(config);
+
 var appHome = angular.module('appHome', []);
 
-    appHome.controller('Entry', function ($scope) {
-        $scope.btnEntry = function () {
-            email = $scope.email;
-            pass = $scope.pass;
-            console.log(email, pass);
-            $scope.btn = '';
-            $scope.pass = $scope.btn;
-            firebase.auth().signInWithEmailAndPassword(email, pass).catch(function (error) {
-                if (error.code) {
-                    alert(error.message)
-                }
-            });
-        }
-    });
+appHome.controller('Entry', function ($scope) {
+    $scope.btnEntry = function () {
+        email = $scope.email;
+        pass = $scope.pass;
+        console.log(email, pass);
+        $scope.btn = '';
+        $scope.pass = $scope.btn;
+        firebase.auth().signInWithEmailAndPassword(email, pass).catch(function (error) {
+            if (error.code) {
+                alert(error.message)
+            }
+        });
+    }
+});
