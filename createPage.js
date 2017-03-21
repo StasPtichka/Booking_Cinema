aa2f6de0e8139be0a5c8033098a807015d0a7a59
Unknown <polyanskidmitri@mail.ru>
2017-03-21 20:41:26 +0300

    page-proofs

--- createPage.js
+++ createPage.js
@@ -1,22 +1,22 @@
 //Стас
+// var config = {
+//     apiKey: "AIzaSyB5Wgo5RO_GI5xf6fsaSfH8OdZlx-X7lqw",
+//     authDomain: "simpleservice-778ef.firebaseapp.com",
+//     databaseURL: "https://simpleservice-778ef.firebaseio.com",
+//     storageBucket: "simpleservice-778ef.appspot.com",
+//     messagingSenderId: "698110095322"
+// };
+// firebase.initializeApp(config);
+
+//Дима
 var config = {
-    apiKey: "AIzaSyB5Wgo5RO_GI5xf6fsaSfH8OdZlx-X7lqw",
-    authDomain: "simpleservice-778ef.firebaseapp.com",
-    databaseURL: "https://simpleservice-778ef.firebaseio.com",
-    storageBucket: "simpleservice-778ef.appspot.com",
-    messagingSenderId: "698110095322"
+    apiKey: "AIzaSyAzreIAAimmDA0-jAiqwtQZdx9gasNKONw",
+    authDomain: "booking-48d90.firebaseapp.com",
+    databaseURL: "https://booking-48d90.firebaseio.com",
+    storageBucket: "booking-48d90.appspot.com",
+    messagingSenderId: "272973760259"
 };
 firebase.initializeApp(config);
-
-/*Дима
- var config = {
- apiKey: "AIzaSyAzreIAAimmDA0-jAiqwtQZdx9gasNKONw",
- authDomain: "booking-48d90.firebaseapp.com",
- databaseURL: "https://booking-48d90.firebaseio.com",
- storageBucket: "booking-48d90.appspot.com",
- messagingSenderId: "272973760259"
- };*/
-//firebase.initializeApp(config);
 
 var appCreate = angular.module('appCreate', []);
 
@@ -31,7 +31,6 @@ appCreate.controller('Create', function ($scope) {
         $scope.btn = '';
         $scope.pass = $scope.btn;
         $scope.passRetry = $scope.btn;
-        checked = false;
         $scope.check = true;
 
         if (pass !== passRetry) {
@@ -39,12 +38,11 @@ appCreate.controller('Create', function ($scope) {
         } else {
             firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function (error) {
                 if (error.code) {
-                    alert(error.message);
+                    alert(error.message)
                 } else {
-                    alert("Регистрация аккаунта прошла успешно!!!")
+                    alert("Регистрация аккаунта прошла успешно!!!");
                 }
             });
-
 
             var bookRef = firebase.database().ref("pasport/" + firebase.auth().currentUser.uid);//тут вместо name будет id пользователя
             bookRef.set(
@@ -53,10 +51,8 @@ appCreate.controller('Create', function ($scope) {
                     Surname: surname,
                     DOB: dateOfborn.toDateString(),
                     Mail_user: email
-                });
-
+                }
+            );
         }
-
-
     }
 });
